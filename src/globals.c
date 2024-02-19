f6dff7d9285cf5b328d1163e102eeb1234450aa1
Alan Coopersmith <alan.coopersmith@oracle.com>
2024-02-19 11:56:59 -0800

    unifdef ATTSHAREDLIB
    Was only defined for SGI Irix platforms in the imake configs,
    has never been defined since the conversion to autoconf
    
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>

--- src/globals.c
+++ src/globals.c
@@ -47,21 +47,7 @@ from The Open Group.
 #define SetZero(t,var,z) t var
 #endif
 
-#ifdef ATTSHAREDLIB			/* then need extra variables */
-/*
- * If we need to define extra variables for each global
- */
-#define ZEROINIT(t,var,val) SetZero(t,var,val); \
-  SetZero (long, _libX_##var##Flag, 0); \
-  SetZero (void *, _libX_##var##Ptr, NULL)
-
-#else /* else not ATTSHAREDLIB */
-/*
- * no extra crud
- */
 #define ZEROINIT(t,var,val) SetZero (t, var, val)
-
-#endif /* ATTSHAREDLIB */
 
 
 /*
